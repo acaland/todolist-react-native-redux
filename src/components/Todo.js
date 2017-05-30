0697158f921a02861cd6be1448575ab7ac10e233
Antonio Calanducci <calanducci@etnatraining.it>
2017-05-30 11:25:42 +0200

    Connect toggleTodo action to Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
-const Todo = ({ item }) => (
-  <View style={styles.wrapper}>
-    <Text style={styles.text}>{item.title}</Text>
-  </View>
+const Todo = ({ item, onToggle }) => (
+    <TouchableOpacity style={styles.wrapper}
+      onPress={onToggle}
+      >
+      <Text style={[styles.text,
+          {
+            textDecorationLine: item.done ? 'line-through' : 'none'
+          }]}
+      >{item.title}</Text>
+    </TouchableOpacity>
 );
+
+
+
 
 const styles = {
   wrapper: {
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 import Todo from './Todo';
 import { connect } from 'react-redux';
+import { toggleTodo } from '../actions';
 
-const TodoList = ({ todolist }) => (
+const TodoList = ({ todolist, onToggleTodo }) => (
   <ScrollView style={{ flex: 1 }}>
-    {todolist.map(todo => <Todo key={todo.id} item={todo} />)}
+    {todolist.map(todo =>
+      <Todo
+        key={todo.id}
+        item={todo}
+        onToggle={() => onToggleTodo(todo.id)}
+      />
+      )
+    }
   </ScrollView>
 );
 
@@ -13,4 +21,8 @@ const mapStateToProps = state => ({
   todolist: state.todolist
 });
 
-export default connect(mapStateToProps, null) (TodoList);
+const mapDispatchToProps = dispatch => ({
+  onToggleTodo: (id) => dispatch(toggleTodo(id))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps) (TodoList);
