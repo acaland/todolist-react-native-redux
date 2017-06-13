af3b99963540700aee1fc3c8c3340fe5da851a48
Antonio Calanducci <calanducci@unict.it>
2017-06-13 21:33:06 +0200

    Add thumbnail affianco ad ogni todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet, Image } from "react-native";
 
 const Todo = ({ item, onClick, onLongPress }) => {
-  const isDone = { textDecorationLine: item.done ? 'line-through' : 'none' };
+  const isDone = { textDecorationLine: item.done ? "line-through" : "none" };
   return (
     <TouchableOpacity
       style={styles.wrapper}
       onPress={onClick}
       onLongPress={onLongPress}
-      >
+    >
+      <Image
+        source={{
+          uri: item.image || "https://facebook.github.io/react/img/logo_og.png"
+        }}
+        style={styles.image}
+      />
       <Text style={[styles.text, isDone]}>
         {item.title}
       </Text>
     </TouchableOpacity>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 20
+    fontSize: 20,
+    marginLeft: 10
+    // borderWidth: 1,
+    // borderColor: 'black'
+  },
+  image: {
+    height: 50,
+    width: 50,
+    borderRadius: 25
   },
   wrapper: {
-    margin: 10
+    margin: 10,
+    flexDirection: "row",
+    alignItems: "center"
   }
 });
 
